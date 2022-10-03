03312ddc495b8b25339b53e8e3f37981822c2b3a
naveed ahamed <naveed@parrotanalytics.com>
2022-10-03 19:37:45 +1300

    Minor fix in the sign up

--- src/containers/Signup/index.js
+++ src/containers/Signup/index.js
@@ -12,13 +12,18 @@ export default function SignUp() {
         <div className="flex flex-col">
           <div className="flex justify-center">
             <p className="p-8 md:m-0 font-semibold text-why-passion-heading text-lg md:text-mini-heading-sz">
-              Watch out !!!  here comes the best offer
+              <span className="text-primary">Watch out !</span> {" "} Here comes the best offer
             </p>
           </div>
           <div className="flex flex-col items-center">
-              <p className="font-light text-regular text-mini-regular-sz p-8 md:p-0 md:text-regular-sz ">
-                We work on a first-come, first-served basis, thus we are only now catching up with the first 50 aspirants.
-              </p>
+              <p className="font-light -mt-6 text-regular text-mini-regular-sz p-8 md:p-0 md:text-regular-sz justify-center">
+                We work on a first-come, first-served basis, thus we are only now catching up with the
+              </p> {" "}
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="font-light text-regular text-mini-regular-sz p-8 md:p-0 md:text-regular-sz">
+              first 50 aspirants.
+            </p>
           </div>
           <div className="flex justify-center mt-10 relative">
             <img src={SignUpPop} alt={"signuppop"}/>
@@ -28,7 +33,7 @@ export default function SignUp() {
 
         <div className="flex justify-center mt-10 ">
         <button
-            className="animate-pulse w-50 bg-primary hover:bg-blue-700 text-md text-white font-bold py-2 px-4 rounded-lg bg-gradient-to-r from-buttonGradient0 to-buttonGradient100"
+            className="animate-pulse w-60 bg-primary hover:bg-blue-700 text-md text-white font-bold py-2 px-4 rounded-lg bg-gradient-to-r from-buttonGradient0 to-buttonGradient100"
             type="button"
             onClick={() => setShowModal(true)}
         >
