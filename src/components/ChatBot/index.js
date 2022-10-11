b0a04f24750d472f8986ef704591ef8bfaf2ca2a
HARIMUTHU-M <harimuthutce@gmail.com>
2022-10-11 17:17:12 +0530

    Bounce at Need Help
    Co-authored-by: Sockalingam <Sockalingam29@users.noreply.github.com>

--- src/components/ChatBot/index.js
+++ src/components/ChatBot/index.js
@@ -9,13 +9,13 @@ function ChatBot() {
     <Fragment>
       <button
         onClick={() => setShowModal(true)}
-        className="fixed bottom-0 hidden md:block right-4 bg-primary rounded-t-lg px-5 font-medium max-auto py-2 text-white hover:scale-105 duration-300 "
+        className="animate-bounce fixed bottom-0 hidden md:block right-4 bg-primary rounded-lg px-5 font-medium max-auto py-2 text-white hover:scale-105 duration-300 "
       >
         Need Help? Contact us!
       </button>
       <button
         onClick={() => setShowModal(true)}
-        className="fixed bottom-6 md:hidden right-6 bg-white rounded-full font-medium max-auto p-4 drop-shadow-xl hover:scale-105 duration-300 "
+        className="fixed bottom-6 md:hidden right-6 bg-white rounded-full font-medium max-auto p-4 drop-shadow-xl hover:scale-105 duration-300 animate-bounce "
       >
         <img src={ContactImage} width={40} height={40} alt="Contact Us" />
       </button>
